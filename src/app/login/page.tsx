09f6dd7c8fbf0db42a9560b4f6c61d613474a0cc
arafathsiam99 <siammdarafathislam@gmail.com>
2024-01-02 13:09:18 +0600

    login page updated with jwt decodation

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import Form from "@/components/Forms/Form";
 import FormInput from "@/components/Forms/FormInput";
 import { SubmitHandler } from "react-hook-form";
+import { useUserLoginMutation } from "@/redux/api/authApi";
+import { isLoggedIn, storeUserInfo } from "@/services/auth.service";
 
 type FormValues = {
   id: string;
@@ -12,10 +14,16 @@ type FormValues = {
 };
 
 const LoginPage = () => {
-  const onSubmit: SubmitHandler<FormValues> = (data) => {
+  const [userLogin] = useUserLoginMutation();
+  console.log(isLoggedIn());
+  const onSubmit: SubmitHandler<FormValues> = async (data: any) => {
     try {
-      console.log(data);
-    } catch (err) {}
+      const res = await userLogin({ ...data }).unwrap();
+      storeUserInfo({ accessToken: res?.data?.accessToken });
+      // console.log(res);
+    } catch (err: any) {
+      console.log(err.message);
+    }
   };
   return (
     <Row
