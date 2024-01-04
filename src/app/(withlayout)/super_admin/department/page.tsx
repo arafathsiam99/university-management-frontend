fcae3d309373a3b1d4bb8cedeb5af5ef870998d2
arafathsiam99 <siammdarafathislam@gmail.com>
2024-01-04 12:25:32 +0600

    reusable table created

--- src/app/(withlayout)/super_admin/department/page.tsx
+++ src/app/(withlayout)/super_admin/department/page.tsx
@@ -1,9 +1,53 @@
 "use client";
 import UMBreadCrumb from "@/components/ui/UMBreadCrumb";
+import UMTable from "@/components/ui/UMTable";
 import { Button } from "antd";
 import Link from "next/link";
 
 const ManageDepartmentPage = () => {
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+      //   sorter: true,
+      sorter: (a: any, b: any) => a.age - b.age,
+    },
+    {
+      title: "Action",
+      render: function (data: any) {
+        return (
+          <Button onClick={() => console.log(data)} type="primary" danger>
+            x
+          </Button>
+        );
+      },
+    },
+  ];
+  const tableData = [
+    {
+      key: "1",
+      name: "Arafath Islam Siam",
+      age: 32,
+    },
+    {
+      key: "2",
+      name: "Jim Green",
+      age: 42,
+    },
+  ];
+  const onPaginationChange = (page: number, pageSize: number) => {
+    console.log("Page:", page, "PageSize:", pageSize);
+  };
+  const onTableChange = (pagination: any, filter: any, sorter: any) => {
+    const { order, field } = sorter;
+    // console.log(order, field);
+  };
   return (
     <div>
       <UMBreadCrumb
@@ -18,6 +62,17 @@ const ManageDepartmentPage = () => {
       <Link href="/super_admin/department/create">
         <Button type="primary">Create</Button>
       </Link>
+      <UMTable
+        loading={false}
+        columns={columns}
+        dataSource={tableData}
+        pageSize={5}
+        totalPages={100}
+        showSizeChanger={true}
+        onPaginationChange={onPaginationChange}
+        onTableChange={onTableChange}
+        showPagination={true}
+      />
     </div>
   );
 };
